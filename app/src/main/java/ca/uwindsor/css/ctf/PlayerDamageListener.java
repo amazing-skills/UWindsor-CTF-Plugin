c55862a7310319bc4ced6687d26326f6fff202bb
trickypr <trickypr@icloud.com>
2023-01-07 14:43:16 +1100

    Fix a null error
    This was the cause of the following error in the console:
    ```
    java.lang.NullPointerException: Cannot invoke "Object.equals(Object)" because the return value of "ca.uwindsor.css.ctf.TeamManager.getPlayerTeam(org.bukkit.entity.Player)" is null
    ```
    
    This should not normally be a problem if everyone on a server is participating in CTF

--- app/src/main/java/ca/uwindsor/css/ctf/PlayerDamageListener.java
+++ app/src/main/java/ca/uwindsor/css/ctf/PlayerDamageListener.java
@@ -12,40 +12,42 @@ import org.bukkit.scheduler.BukkitScheduler;
 import net.md_5.bungee.api.ChatColor;
 
 public class PlayerDamageListener implements Listener {
-	
+
 	@EventHandler
 	public void onDamage(EntityDamageByEntityEvent event) {
-		
-		//Check for team-hitting
-		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {	//If the attacker and victim are players
-			
+
+		// Check for team-hitting
+		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) { // If the attacker and victim
+																							// are players
+
 			Player attacker = (Player) event.getDamager();
 			Player victim = (Player) event.getEntity();
-			
-			if (TeamManager.getPlayerTeam(attacker).equals(TeamManager.getPlayerTeam(victim))) {
+
+			Team attackerTeam = TeamManager.getPlayerTeam(attacker);
+			Team victimTeam = TeamManager.getPlayerTeam(victim);
+
+			// If neither player is on a team, it will return null, which isn't
+			// the best for a clean console output
+			if (attackerTeam == null || victimTeam == null) {
+				return;
+			}
+
+			if (attackerTeam.equals(victimTeam)) {
 				attacker.sendMessage(ChatColor.RED + "You cannot attack your own team members!");
 				event.setCancelled(true);
 				return;
 			}
 
+			// Check if pvp is enabled
+			if (Main.pvpEnabled == false) {
+				event.setCancelled(true);
+				event.getDamager().sendMessage(ChatColor.RED + "PvP is currently disabled!");
+				return;
+			}
+
 		}
-		
-		//Check if PVP is enabled
-		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player && Main.pvpEnabled == false) {
-			event.setCancelled(true);
-			event.getDamager().sendMessage(ChatColor.RED + "PvP is currently disabled!");
-			return;
-		}
-		
-		//Check if PvP is inside spawn
-		if (event.getEntity() instanceof Player && event.getEntity().getLocation().distance(event.getEntity().getWorld().getSpawnLocation()) < 30) {
-			event.getDamager().sendMessage(ChatColor.RED + "PvP is disabled in spawn!");
-			event.setCancelled(true);
-			return;
-		}
-		
 	}
-	
+
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event) {
 		Player player = event.getEntity();
